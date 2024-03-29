b278f721728e32ed21a1a28de35c8ad658295633
Wulius <werbesan@gmail.com>
2024-03-29 11:18:21 +0100

    Add handleUpload function and cloudinary package

--- app/hotels/dashboard/HotelsForm.tsx
+++ app/hotels/dashboard/HotelsForm.tsx
@@ -1,17 +1,10 @@
 'use client';
 
 import { CldUploadButton } from 'next-cloudinary';
-// import dayjs from 'dayjs';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
-import ImageUpload from '../../../components/ImageUploader';
 import { RegisterResponseBodyPost } from '../../api/hotels/route';
-// import { Animal } from '../../../migrations/00000-createTableAnimals';
 import ErrorMessage from '../../ErrorMessage';
-
-// type Props = {
-//   animals: Animal[];
-// };
 
 export default function HotelsForm() {
   const [hotelName, setHotelName] = useState('');
@@ -19,6 +12,7 @@ export default function HotelsForm() {
   const [address, setAddress] = useState('');
   const [rating, setRating] = useState(0);
   const [pricePerNight, setPricePerNight] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
   const [errors, setErrors] = useState<{ message: string }[]>([]);
 
   const router = useRouter();
@@ -33,6 +27,7 @@ export default function HotelsForm() {
         address: address,
         rating: rating,
         pricePerNight: pricePerNight,
+        imageUrl: imageUrl, // Include the uploaded image URL in your request payload
       }),
       headers: {
         'Content-Type': 'application/json',
@@ -47,28 +42,16 @@ export default function HotelsForm() {
     }
     router.push(`/hotels/${data.hotel.hotelName}`);
   }
-  // Weiterleitung zur designten Komponente -- später
-  // router.push(
-  //   // getSafeReturnToPath(props.returnTo)
-  //   `/profile/${data.user.username}`,
-  // );
-  //   router.refresh();
-  // }
 
-  // Reset form states to default values so that the form is
-  // cleared after an add, edit or delete action
-  function resetFormStates() {
-    setHotelName('');
-    setDescription('');
-    setAddress('');
-    setRating(0);
-    setPricePerNight('');
+  function handleUploadSuccess(response: any) {
+    setImageUrl(response.info.secure_url);
   }
+  console.log(imageUrl);
 
   return (
-    <section className=" flex justify-center dark:bg-gray-900 mt-8">
-      <div className=" flex flex-col justify-center p-6 space-y-4 md:space-y-6 sm:p-8 lg:w-2/5 md:w-3/5 w-4/5">
-        <h1 className=" text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white text-center">
+    <section className="flex justify-center dark:bg-gray-900 mt-8">
+      <div className="flex flex-col justify-center p-6 space-y-4 md:space-y-6 sm:p-8 lg:w-2/5 md:w-3/5 w-4/5">
+        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white text-center">
           Create a Hotel
         </h1>
         <form
@@ -133,14 +116,19 @@ export default function HotelsForm() {
             </div>
             <input
               className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="€ 120,-"
+              placeholder="120"
               onChange={(event) => setPricePerNight(event.currentTarget.value)}
             />
           </div>
-          <CldUploadButton uploadPreset="<Upload Preset>" />
-
-          <ImageUpload />
-          <div className="text-center">
+          <div className="flex justify-center">
+            <div className="text-center text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 w-1/3">
+              <CldUploadButton
+                onSuccess={handleUploadSuccess} // Handle upload success
+                uploadPreset="bookingwulf"
+              />
+            </div>
+          </div>
+          <div className="text-center ">
             <button
               // type="button"
               className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
